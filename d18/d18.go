08829b671112ffeca6566da03f0975ffbb4215f1
Joe Mou <dev@mou.fo>
2020-12-18 03:25:08 -0800

    go d18 p2

--- d18/d18.go
+++ d18/d18.go
@@ -13,11 +13,11 @@ type parser struct {
 
 func NewParser(in []byte) parser {
 	// no built-in reverse
-	out := make([]byte, len(in))
-	for i, ch := range in {
-		out[len(out)-i-1] = ch
-	}
-	return parser{out}
+	// out := make([]byte, len(in))
+	// for i, ch := range in {
+	// 	out[len(out)-i-1] = ch
+	// }
+	return parser{in}
 }
 
 func (p *parser) readByte() (byte, error) {
@@ -45,77 +45,112 @@ func (p *parser) parseNumber() (int, error) {
 	return int(num - '0'), err
 }
 
-func (p *parser) ParseAtom() (int, error) {
+func (p *parser) ParseAtom() (int, int, error) {
 	if len(p.in) == 0 {
-		return 0, errors.New("unexpected END")
-	} else if p.in[0] == ')' {
+		return 0, 0, errors.New("unexpected END")
+	} else if p.in[0] == '(' {
 		p.in = p.in[1:]
-		value, err := p.ParseExpr()
+		p1, p2, err := p.ParseExpr()
 		if err != nil {
-			return 0, err
+			return 0, 0, err
 		}
 		paren, err := p.readByte()
 		if err != nil {
-			return 0, err
+			return 0, 0, err
 		}
-		if paren != '(' {
-			return 0, errors.New("expected (")
+		if paren != ')' {
+			return 0, 0, errors.New("expected (")
 		}
-		return value, nil
+		return p1, p2, nil
 	} else {
 		value, err := p.parseNumber()
 		if err != nil {
-			return 0, err
+			return 0, 0, err
 		}
-		return value, nil
+		return value, value, nil
 	}
 }
 
-func (p *parser) ParseExpr() (int, error) {
-	right, err := p.ParseAtom()
+func (p *parser) ParseExpr() (int, int, error) {
+	var atomsP1, atomsP2 []int
+	p1, p2, err := p.ParseAtom()
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
-	if len(p.in) == 0 || p.in[0] == '(' {
-		return right, nil
+	atomsP1 = append(atomsP1, p1)
+	atomsP2 = append(atomsP2, p2)
+	for len(p.in) > 0 && p.in[0] != ')' {
+		if err := p.chompSpace(); err != nil {
+			return 0, 0, err
+		}
+		op, err := p.readByte()
+		if err != nil {
+			return 0, 0, err
+		}
+		if err := p.chompSpace(); err != nil {
+			return 0, 0, err
+		}
+		atomsP1 = append(atomsP1, int(op))
+		atomsP2 = append(atomsP2, int(op))
+		p1, p2, err := p.ParseAtom()
+		if err != nil {
+			return 0, 0, err
+		}
+		atomsP1 = append(atomsP1, p1)
+		atomsP2 = append(atomsP2, p2)
 	}
-	if err := p.chompSpace(); err != nil {
-		return 0, err
+
+	p1 = atomsP1[0]
+	for i := 1; i < len(atomsP1); i += 2 {
+		switch atomsP1[i] {
+		case '+':
+			p1 += atomsP1[i+1]
+		case '*':
+			p1 *= atomsP1[i+1]
+		default:
+			if len(atomsP1) > 1 {
+				return 0, 0, errors.New("unknown operator")
+			}
+		}
 	}
-	op, err := p.readByte()
-	if err != nil {
-		return 0, err
+
+	// answer too low
+	j := 1
+	for i := 1; i < len(atomsP2); i += 2 {
+		if atomsP2[i] == '+' {
+			atomsP2[j-1] = atomsP2[j-1] + atomsP2[i+1]
+		} else {
+			atomsP2[j] = atomsP2[i]
+			atomsP2[j+1] = atomsP2[i+1]
+			j += 2
+		}
 	}
-	if err := p.chompSpace(); err != nil {
-		return 0, err
+	atomsP2 = atomsP2[:j]
+
+	p2 = 1
+	// operators must all be '*'
+	for i := 0; i < len(atomsP2); i += 2 {
+		p2 *= atomsP2[i]
 	}
-	left, err := p.ParseExpr()
-	if err != nil {
-		return 0, err
-	}
-	switch op {
-	case '+':
-		return left + right, nil
-	case '*':
-		return left * right, nil
-	default:
-		return 0, errors.New("expected OPERATOR")
-	}
+
+	return p1, p2, nil
 }
 
 func main() {
-	sum := 0
+	sumP1, sumP2 := 0, 0
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		parser := NewParser(scanner.Bytes())
-		value, err := parser.ParseExpr()
+		p1, p2, err := parser.ParseExpr()
 		if err != nil {
 			panic(err)
 		}
-		sum += value
+		sumP1 += p1
+		sumP2 += p2
 	}
 	if scanner.Err() != nil {
 		panic(scanner.Err())
 	}
-	fmt.Println(sum)
+	fmt.Println(sumP1)
+	fmt.Println(sumP2)
 }
